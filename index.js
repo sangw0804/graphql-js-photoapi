927984aceb78ccb80c2fcee2390654f066bdbcfe
sw-oh <sangw0804@sangw0804ui-MacBookPro.local>
2021-02-21 00:34:53 +0900

    allPhotos 추가

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const { ApolloServer } = require('apollo-server');
 
+let _id = 0;
+
 const photos = [];
 
 const typeDefs = `
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
     }
 
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(name: String! description: String): Photo!
+    }
+
+    type Photo {
+        id: ID!
+        name: String!
+        url: String!
+        description: String
     }
 `;
 
 const resolvers = {
     Query: {
-        totalPhotos: () => photos.length
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
     Mutation: {
         postPhoto: (parent, args) => {
-            photos.push(args);
+            const created = {
+                id: _id++,
+                ...args
+            };
 
-            return true;
+            photos.push(created);
+
+            return created;
         }
+    },
+    Photo: {
+        url: parent => `http://test.com/photos/${parent.id}.jpg`
     }
 };
 
